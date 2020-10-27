86bd1edc1142b5b88b463b58d83654e68963080d
Alexey Levchuk <bizlex@yandex.ru>
2020-10-27 10:43:33 +0300

    Add master step 1-4

--- force-app/main/default/aura/autoLoan/autoLoanController.js
+++ force-app/main/default/aura/autoLoan/autoLoanController.js
@@ -1,114 +1,138 @@
 ({
-    nextTab : function(component, event, helper) {
-        component.set("v.setMessage", '');           
-        var showAccount = component.get("v.showAccount");
-        var showContact = component.get("v.showContact");
-        var showOpportunity = component.get("v.showOpportunity");
-        var showData = component.get("v.showData");
-        
-        if(showAccount == true){
-            var accountName = component.find("Name").get("v.value");
-            console.log('accountName:::'+accountName);
-            if(accountName =='' || accountName == null) {
-                component.set("v.setMessage",'error');           
-            }
-            if(component.get("v.setMessage")=='error') { 
-                component.set("v.showContact",false);
-                component.set("v.showOpportunity", false);
-                component.set("v.showError", true);
-                component.set("v.showData", false);
-                
-            }else{ 
-                component.set("v.showAccount", false);
-                component.set("v.showContact", true);
-                component.set("v.showOpportunity", false); 
-                component.set("v.showError", false);
-                component.set("v.showData", false);
-            }
-        } 
+    nextTab: function (component, event, helper) {
+        component.set("v.setMessage", "");
+        var firstStep = component.get("v.firstStep");
+        var secondStep = component.get("v.secondStep");
+        var thirdStep = component.get("v.thirdStep");
+        console.log('firstStep = ', firstStep);
+        console.log('secondStep = ', secondStep);
+        console.log('thirdStep = ', thirdStep);
+        var fourthStep = component.get("v.fourthStep");
 
-        if(showContact == true){
-            var lastName = component.find("LastName").get("v.value");
-            console.log('lastName:::'+lastName);
-            if(lastName =='' || lastName == null) {
-                component.set("v.setMessage",'error');           
+        if (firstStep == true) {
+            var firstName = component.find("firstName").get("v.value");
+            console.log("firstName:::" + firstName);
+            if (firstName == "" || firstName == null) {
+                component.set("v.setMessage", "error");
             }
-            if(component.get("v.setMessage")=='error') { 
-                component.set("v.showAccount", false);
-                component.set("v.showOpportunity", false);
+            if (component.get("v.setMessage") == "error") {
+                component.set("v.secondStep", false);
+                component.set("v.thirdStep", false);
                 component.set("v.showError", true);
-                component.set("v.showData", false);
-            }else{ 
-                component.set("v.showAccount", false);
-                component.set("v.showContact", false);
-                component.set("v.showOpportunity", true);
+                component.set("v.fourthStep", false);
+            } else {
+                component.set("v.firstStep", false);
+                component.set("v.secondStep", true);
+                component.set("v.thirdStep", false);
                 component.set("v.showError", false);
-                component.set("v.showData", false);
+                component.set("v.fourthStep", false);
             }
-        }   
-        
-        if(showOpportunity == true){
-            var OpportunityName = component.find("OpportunityName").get("v.value");
-            console.log('OpportunityName:::'+OpportunityName);
-            var StageName = component.find("StageName").get("v.value");
-            console.log('StageName:::'+StageName);
-            var closeDate = component.find("closeDate").get("v.value");
-            console.log('closeDate:::'+closeDate);
-            if((OpportunityName =='' || OpportunityName == null) || (StageName =='' || StageName == null) || (closeDate =='' || closeDate == null)) {
-                component.set("v.setMessage",'error');           
+        }
+
+        if (secondStep == true) {
+            var seriesPassport = component.find("seriesPassport").get("v.value");
+            console.log("seriesPassport:::" + seriesPassport);
+            if (seriesPassport == "" || seriesPassport == null) {
+                component.set("v.setMessage", "error");
             }
-            if(component.get("v.setMessage")=='error') { 
-                component.set("v.showAccount", false);
-                component.set("v.showContact", false);
+            if (component.get("v.setMessage") == "error") {
+                component.set("v.firstStep", false);
+                component.set("v.thirdStep", false);
                 component.set("v.showError", true);
-                component.set("v.showData", false); 
-            }else{ 
-                component.set("v.showOpportunity", false);
-                component.set("v.showContact", false);
-                component.set("v.showAccount", false)
+                component.set("v.fourthStep", false);
+            } else {
+                component.set("v.firstStep", false);
+                component.set("v.secondStep", false);
+                component.set("v.thirdStep", true);
                 component.set("v.showError", false);
-                component.set("v.showData", true);
+                component.set("v.fourthStep", false);
             }
-        }   
-        
+        }
+
+        if (thirdStep == true) {
+            var firstNameContact = component.find("firstNameContact").get("v.value");
+            console.log("firstNameContact:::" + firstNameContact);
+            if (
+                firstNameContact == "" ||
+                firstNameContact == null
+            ) {
+                component.set("v.setMessage", "error");
+            }
+            if (component.get("v.setMessage") == "error") {
+                component.set("v.firstStep", false);
+                component.set("v.secondStep", false);
+                component.set("v.showError", true);
+                component.set("v.fourthStep", false);
+            } else {
+                component.set("v.thirdStep", false);
+                component.set("v.secondStep", false);
+                component.set("v.firstStep", false);
+                component.set("v.showError", false);
+                component.set("v.fourthStep", true);
+            }
+        }
     },
 
-    prevTab : function(component, event, helper) {
-        var showAccount = component.get("v.showAccount");
-        var showContact = component.get("v.showContact");
-        var showOpportunity = component.get("v.showOpportunity");
-        var showData = component.get("v.showData");
-        
-        if(showContact == true) {
-            component.set("v.showAccount", true);
-            component.set("v.showContact", false);
-            component.set("v.showOpportunity", false);
+    prevTab: function (component, event, helper) {
+        var firstStep = component.get("v.firstStep");
+        var secondStep = component.get("v.secondStep");
+        var thirdStep = component.get("v.thirdStep");
+        var fourthStep = component.get("v.fourthStep");
+        console.log('firstStep prevTab= ', firstStep);
+        console.log('secondStep prevTab= ', secondStep);
+        console.log('thirdStep prevTab= ', thirdStep);
+
+        if (secondStep == true) {
+            component.set("v.firstStep", true);
+            component.set("v.secondStep", false);
+            component.set("v.thirdStep", false);
             component.set("v.showError", false);
-            component.set("v.showData", false);
-        }    
-        if(showOpportunity == true) {
-            component.set("v.showAccount", false);
-            component.set("v.showContact", true);
-            component.set("v.showOpportunity", false);
+            component.set("v.fourthStep", false);
+        }
+        if (thirdStep == true) {
+            component.set("v.firstStep", false);
+            component.set("v.secondStep", true);
+            component.set("v.thirdStep", false);
             component.set("v.showError", false);
-            component.set("v.showData", false);
-        } 
-        if(showData == true) {
-            component.set("v.showAccount", false);
-            component.set("v.showContact", false);
-            component.set("v.showOpportunity", true);
+            component.set("v.fourthStep", false);
+        }
+        if (fourthStep == true) {
+            component.set("v.firstStep", false);
+            component.set("v.secondStep", false);
+            component.set("v.thirdStep", true);
             component.set("v.showError", false);
-            component.set("v.showData", false);
-        }  
+            component.set("v.fourthStep", false);
+        }
     },
-    
-    onSelectChange : function(component, event, helper) {
+
+    onSelectChange: function (component, event, helper) {
         var selected = component.find("StageName").get("v.value");
-        component.set("v.OpportunityData.StageName",selected);
-        console.log('opp::::'+JSON.stringify(selected));
+        component.set("v.OpportunityData.StageName", selected);
+        console.log("opp::::" + JSON.stringify(selected));
+    },
+
+    saveRecord: function (component, event, helper) {
+        helper.saveData(component, event, helper);
+    },
+
+    changeCreditTerm: function (cmp, event) {
+        console.log(cmp.get("v.creditTerm"));
+    },
+
+    changeStatusMarriage: function(component, event, helper) {
+		var changeValue = event.getParam("value");
+		component.set("v.statusMarriageValue", changeValue);
+        var statusMarriageValue = component.get("v.statusMarriageValue");
+        console.log('statusMarr = ', statusMarriageValue);
     },
     
-    saveRecord : function(component, event, helper) {
-        helper.saveData(component, event, helper);               
-    }
-})
+    changeStatusSocial: function(component, event, helper) {
+        var changeValue = event.getParam("value");
+        console.log('changeValue = ', changeValue); 
+    },
+
+    changeformBusiness: function(component, event, helper) {
+        var changeValue = event.getParam("value");
+        console.log('changeValue = ', changeValue); 
+	},
+});
